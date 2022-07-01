44e960868d39ac49fc21b242e433f2c98616a8c0
Camilo Hernandez Echeverri <1152464009@medellin.gov.co>
2022-07-01 08:44:21 -0500

    finalizando el taller

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-//1er paso
+//1er paso - capturar nombre y apellido
 
 const nombre = prompt("Ingresa tu nombre");
 const apellido = prompt("Ingresa tu apellido");
@@ -6,42 +6,53 @@ const apellido = prompt("Ingresa tu apellido");
 /* console.log(nombre)
 console.log(apellido) */
 
-//2do paso
+//2do paso - capurar primera inicial de nombre y primera inicial de apellido en mayúscula.
 
-const inicialNombre = nombre.charAt(0);
-const inicialApellido = apellido.charAt(0);
+const inicialNombre = nombre.charAt(0).toUpperCase();
+const inicialApellido = apellido.charAt(0).toUpperCase();
 
 /* console.log(inicialNombre)
 console.log(inicialApellido) */
 
-//3er paso
+//3er paso - crear etiqueta H2 con Js
 
 const bienvenida = document.createElement("h2");
 bienvenida.textContent = `Bienvendio: ${inicialNombre} ${inicialApellido}`;
 
 /* console.log(bienvenida) */
 
-//4to paso
+//4to paso - inyectar texto al H2.
 
 const saludo = document.querySelector("#bienvenido");
 saludo.appendChild(bienvenida)
 
-//5to paso
+//5to paso - crear lista
 
 let listaProducto = [];
+
+//9no paso - Refrescar Aviso
+
+document.querySelector("#aviso").innerHTML=""
+
+//10mo paso - convertir texto a numeros con parseInt.
+
 function guardar(){
     const code = document.querySelector("#codigo").value;
     const product = document.querySelector("#producto").value;
-    const price = document.querySelector("#precio").value;
+    const price = parseInt(document.querySelector("#precio").value);
     
     /*  console.log(code)
     console.log(product)
     console.log(price) */
     
+    //11avo paso - crear nueva columna para el precio más IVA y adicional el precio total con IVA.
+
     const producto = {
         codigo:code,
         producto:product,
         precio:price,
+        iva:price*0.19,
+        total:price+(price*0.19),
     }
 
     /* console.log(producto) */
@@ -50,15 +61,23 @@ function guardar(){
 
     /* console.log(listaProducto) */
 
+    //6to paso - Ingreso de producto
+
     const productoAviso = document.createElement("h3");
     productoAviso.textContent = `Acabas de ingresar el producto: ${producto.producto}`;
 
     const aviso = document.querySelector("#aviso");
-    aviso.appendChild(productoAviso)
+    aviso.appendChild(productoAviso);
 
-    console.table(listaProducto)
+    /* console.table(listaProducto) */
+
+    //7mo paso - Borrador del formulario
+
+    document.querySelector("#codigo").value="";
+    document.querySelector("#producto").value="";
+    document.querySelector("#precio").value="";
 }
 
-//6to paso
+
 
 
